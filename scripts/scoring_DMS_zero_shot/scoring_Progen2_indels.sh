4df396c48ea4cea46ceac2412f4ccb2bac156b03
anon <edamame_descent.0m@icloud.com>
2023-08-16 23:07:29 -0400

    Fixing indels reference file

--- scripts/scoring_DMS_zero_shot/scoring_Progen2_indels.sh
+++ scripts/scoring_DMS_zero_shot/scoring_Progen2_indels.sh
@@ -1,31 +1,33 @@
 #!/bin/bash 
-#SBATCH --cpus-per-task=5
+#SBATCH --cpus-per-task=1
 #SBATCH --gres=gpu:1
-#SBATCH -p gpu_quad
-#SBATCH -t 48:00:00
-#SBATCH --mem=90G
-#SBATCH --output=../../slurm_stdout/slurm-%j.out
-#SBATCH --error=../../slurm_stdout/slurm-%j.err
+#SBATCH -p gpu_quad,gpu,gpu_marks
+#SBATCH --qos=gpuquad_qos
+#SBATCH -t 0:45:00
+#SBATCH --mem=32G
+#SBATCH --output=./slurm/progen2_indels/slurm-%j.out
+#SBATCH --error=./slurm/progen2_indels/slurm-%j.err
 #SBATCH --job-name="ProBFD_base"
-#SBATCH --array=0-2,4-6,12-21
+# #SBATCH --array=0-2,4-6,12-21
+#SBATCH --array=8,9,11-74
 
 module load conda2/4.2.13
 module load gcc/6.2.0
 module load cuda/10.1
 
-source activate protein_transformer_env
+source activate /n/groups/marks/software/anaconda_o2/envs/indels_env
 
 #export Progen2_model_name_or_path="/n/groups/marks/projects/marks_lab_and_oatml/protein_transformer/baseline_models/progen2/progen2-small"
 #export output_scores_folder="/n/groups/marks/projects/marks_lab_and_oatml/protein_transformer/model_scores/progen2/progen2-small/indels"
 #export performance_file="/home/pn73/protein_transformer/outputs/performance_proteingym/progen2/progen2_small_indels.csv"
 
-#export Progen2_model_name_or_path="/n/groups/marks/projects/marks_lab_and_oatml/protein_transformer/baseline_models/progen2/progen2-medium"
-#export output_scores_folder="/n/groups/marks/projects/marks_lab_and_oatml/protein_transformer/model_scores/progen2/progen2-medium/indels"
-#export performance_file="/home/pn73/protein_transformer/outputs/performance_proteingym/progen2/progen2_medium_indels.csv"
+export Progen2_model_name_or_path="/n/groups/marks/projects/marks_lab_and_oatml/protein_transformer/baseline_models/progen2/progen2-medium"
+export output_scores_folder="/n/groups/marks/projects/marks_lab_and_oatml/ProteinGym/model_scores/zero_shot_indels/v2_indel_scores/Progen2/medium"
+export performance_file="/n/groups/marks/projects/marks_lab_and_oatml/ProteinGym/performance_files/progen2_medium_indels.csv"
 
-export Progen2_model_name_or_path="/n/groups/marks/projects/marks_lab_and_oatml/protein_transformer/baseline_models/progen2/progen2-base"
-export output_scores_folder="/n/groups/marks/projects/marks_lab_and_oatml/protein_transformer/model_scores/progen2/progen2-base/indels_v2"
-export performance_file="/home/pn73/protein_transformer/outputs/performance_proteingym/progen2/progen2_base_indels2.csv"
+# export Progen2_model_name_or_path="/n/groups/marks/projects/marks_lab_and_oatml/protein_transformer/baseline_models/progen2/progen2-base"
+# export output_scores_folder="/n/groups/marks/projects/marks_lab_and_oatml/protein_transformer/model_scores/progen2/progen2-base/indels_v2"
+# export performance_file="/home/pn73/protein_transformer/outputs/performance_proteingym/progen2/progen2_base_indels2.csv"
 
 #export Progen2_model_name_or_path="/n/groups/marks/projects/marks_lab_and_oatml/protein_transformer/baseline_models/progen2/progen2-large"
 #export output_scores_folder="/n/groups/marks/projects/marks_lab_and_oatml/protein_transformer/model_scores/progen2/progen2-large/indels"
@@ -46,10 +48,10 @@ export performance_file="/home/pn73/protein_transformer/outputs/performance_prot
 #export DMS_reference_file_path='/home/pn73/Tranception/proteingym/ProteinGym_reference_file_indels.csv'
 #export DMS_data_folder='/n/groups/marks/projects/marks_lab_and_oatml/protein_transformer/Tranception_open_source/DMS_files/ProteinGym_indels'
 
-export DMS_reference_file_path='/home/pn73/Tranception/examples_local/ProteinGym_reference_file_indels_speedups.csv'
-export DMS_data_folder='/n/groups/marks/projects/marks_lab_and_oatml/protein_transformer/Tranception_open_source/DMS_files/ProteinGym_speedups/indels'
+export DMS_reference_file_path='/n/groups/marks/projects/marks_lab_and_oatml/ProteinGym/reference_files/DMS_indels_reference_file_reduced_2023_08_16.csv'
+export DMS_data_folder='/n/groups/marks/projects/marks_lab_and_oatml/ProteinGym/DMS_assays/indels_new'
 
-srun python3 compute_fitness.py \
+srun python3 ../../proteingym/baselines/progen2/compute_fitness.py \
             --Progen2_model_name_or_path ${Progen2_model_name_or_path} \
             --DMS_reference_file_path ${DMS_reference_file_path} \
             --DMS_data_folder ${DMS_data_folder} \
